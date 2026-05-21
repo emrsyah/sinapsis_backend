1892e5966ce1e9b041535828760809abe2f3158c
shadrairfn <shadrairfn@gmail.com>
2026-05-21 13:05:21 +0700

    chore: remove websocket-test view and route

--- routes/web.php
+++ routes/web.php
@@ -10,18 +10,4 @@ Route::get('/', function () {
 
 Route::get('auth/google/callback', [AuthController::class, 'handleGoogleCallback']);
 
-Route::get('/websocket-test', function () {
-    $user = \App\Models\User::firstOrCreate(
-        ['email' => 'test@example.com'],
-        ['name' => 'Test User']
-    );
-    
-    $note = \App\Models\Note::firstOrCreate(
-        ['user_id' => $user->user_id],
-        ['title' => 'Test Note', 'content' => 'Test Content']
-    );
 
-    Auth::login($user);
-
-    return view('websocket-test', ['note' => $note]);
-});
